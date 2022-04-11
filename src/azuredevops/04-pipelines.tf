07e6aa1011317acd118d3cd2dc0def8000ed7334
uolter <walter.traspadini@pagopa.it>
2022-04-11 10:42:08 +0200

    pipeline to deploy user registry

--- src/azuredevops/04-pipelines.tf
+++ src/azuredevops/04-pipelines.tf
@@ -5,9 +5,14 @@ locals {
   ms_person_app = {
     docker_image = "ms-person"
   }
+
+  ms_user_resistry_app = {
+    docker_image = "ms-user-registry"
+  }
 }
 
 
+# MS tokenizer
 module "ecs_tokenizer_deploy" {
   source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v2.0.5"
 
@@ -27,6 +32,7 @@ module "ecs_tokenizer_deploy" {
   ]
 }
 
+# MS Person
 module "ecs_person_deploy" {
   source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v2.0.5"
 
@@ -37,6 +43,27 @@ module "ecs_person_deploy" {
   ci_trigger_use_yaml = true
 
   variables        = local.ms_person_app
+  variables_secret = {}
+
+  service_connection_ids_authorization = [
+    azuredevops_serviceendpoint_github.github_ro.id,
+    azuredevops_serviceendpoint_aws.uat_serviceendpoint.id,
+    azuredevops_serviceendpoint_aws.prod_serviceendpoint.id,
+  ]
+}
+
+# MS User registry
+
+module "ecs_user_registry_deploy" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v2.0.5"
+
+  project_id                   = azuredevops_project.this.id
+  repository                   = var.ms-user-registry.repository
+  github_service_connection_id = azuredevops_serviceendpoint_github.github_pr.id
+
+  ci_trigger_use_yaml = true
+
+  variables        = local.ms_user_resistry_app
   variables_secret = {}
 
   service_connection_ids_authorization = [
