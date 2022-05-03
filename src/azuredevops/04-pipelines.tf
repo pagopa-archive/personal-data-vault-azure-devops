a4851ba3f871b1517f52a28043b404379284eb0d
uolter <walter.traspadini@pagopa.it>
2022-05-03 08:46:27 +0200

    removed tokenizer pipeline

--- src/azuredevops/04-pipelines.tf
+++ src/azuredevops/04-pipelines.tf
@@ -1,7 +1,4 @@
 locals {
-  ms_tokenizer_app = {
-    docker_image = "ms-tokenizer"
-  }
   ms_person_app = {
     docker_image = "ms-person"
   }
@@ -16,25 +13,7 @@ locals {
 }
 
 
-# MS tokenizer
-module "ecs_tokenizer_deploy" {
-  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v2.0.5"
 
-  project_id                   = azuredevops_project.this.id
-  repository                   = var.ms-tokenizer.repository
-  github_service_connection_id = azuredevops_serviceendpoint_github.github_pr.id
-
-  ci_trigger_use_yaml = true
-
-  variables        = local.ms_tokenizer_app
-  variables_secret = {}
-
-  service_connection_ids_authorization = [
-    azuredevops_serviceendpoint_github.github_ro.id,
-    azuredevops_serviceendpoint_aws.uat_serviceendpoint.id,
-    azuredevops_serviceendpoint_aws.prod_serviceendpoint.id,
-  ]
-}
 
 # MS Person
 module "ecs_person_deploy" {
